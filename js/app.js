ca5252bead3cdfc55d3954bc9eecd2106bb55ff1
ochekrigin <chekriginoleg@gmail.com>
2016-08-17 17:40:34 +0300

    Added fixes to table

--- js/app.js
+++ js/app.js
@@ -1,15 +1,18 @@
 (function () {
 	'use strict'
 
-	var table = document.querySelectorAll('.table'),
-		thead = table[0].tHead,
+	var table = document.getElementById('table'),
+		thead = table.tHead,
 		tCells = thead.rows[0].cells,
-		inputCollection = document.querySelectorAll('input[type="text"]'),
+		input = document.createElement('input'),
 		buttonCollection = document.querySelectorAll('.btn-delete'),
+		textBlockCollection = document.querySelectorAll('.text'),
 		currentCell,
 		topClass = 'top',
+		inputCollection,
 		bottomClass = 'bottom',
 		editClass = 'edit';
+		input.type = 'text';
 
 	/**
 	* sortTable() sort table's cell
@@ -43,45 +46,57 @@
 	function removeItem (item) {
 		if (item) {
 			var curentRow = item.closest('tr'),
-				currentCheckbox = curentRow.querySelectorAll('input[type="checkbox"]')[0];
+				currentCheckbox = curentRow.querySelector('input[type="checkbox"]');
 			if (currentCheckbox.checked) {
 				curentRow.remove();
 			}
 		}
 	}
 
-	// loop for adding events for sort table
-	for (var j = tCells.length - 1; j >= 0 ; j--) {
-		var currentItem = tCells[j],
-			dir = 1;
-		(function (j){
-			currentItem.addEventListener('click', function () {
-				if (this.classList.contains(topClass)) {
-					this.classList.remove(topClass);
-					this.classList.add(bottomClass);
-				} else {
-					this.classList.remove(bottomClass);
-					this.classList.add(topClass);
-				}
-	        	sortTable(table[0], j, (dir = 1 - dir))
-	        });
-		})(j)
+	function appendInput () {
+		for (var i = 0; i < textBlockCollection.length; i++) {
+			var inputClone = input.cloneNode(true);
+			textBlockCollection[i].appendChild(inputClone);
+		}
+		inputCollection = document.querySelectorAll('input[type="text"]');
 	}
 
-	// loop for adding events for edit table cell
-	for (var i = 0; i < inputCollection.length; i++) {
-		var currentInput = inputCollection[i];
-		currentInput.addEventListener('focus', function () {
-			this.value = currentCell.getElementsByTagName('span')[0].innerText;
-		});
-		currentInput.addEventListener('blur', function () {
-			currentCell.getElementsByTagName('span')[0].innerText = this.value;
-			currentCell.classList.remove(editClass);
-		});
+	//function adding events for sort table
+	function eventSortTable () {
+		for (var j = tCells.length - 1; j >= 0 ; j--) {
+			var currentItem = tCells[j],
+				dir = 1;
+			(function (j){
+				currentItem.addEventListener('click', function () {
+					if (this.classList.contains(topClass)) {
+						this.classList.remove(topClass);
+						this.classList.add(bottomClass);
+					} else {
+						this.classList.remove(bottomClass);
+						this.classList.add(topClass);
+					}
+		        	sortTable(table, j, (dir = 1 - dir))
+		        });
+			})(j)
+		}
 	}
 
-	// event for showing edit input
-	table[0].addEventListener('dblclick', function(event) {
+	// function adding events for edit table cell
+	function eventEditTable () {
+		for (var i = 0; i < inputCollection.length; i++) {
+			var currentInput = inputCollection[i];
+			currentInput.addEventListener('focus', function () {
+				this.value = currentCell.getElementsByTagName('span')[0].innerText;
+			});
+			currentInput.addEventListener('blur', function () {
+				currentCell.getElementsByTagName('span')[0].innerText = this.value;
+				currentCell.classList.remove(editClass);
+			});
+		}
+	}
+
+	// function removing row from table
+	function eventInputEdit (event) {
 		currentCell = event.target.closest('td');
 		if (currentCell) {
 			var currentInput = currentCell.querySelectorAll('input[type="text"]')[0];
@@ -90,13 +105,28 @@
 				currentInput.focus();
 			}
 		}
-	});
+	}
 
-	// event for row removing
-	table[0].addEventListener('click', function (event) {
+	// function removing row from table
+	function eventRowRemovin (event) {
 		if (event.target.className === buttonCollection[0].className) {
 			removeItem(event.target);
 		}
-	});
+	}
+
+	// event for showing edit input
+	table.addEventListener('dblclick', eventInputEdit);
+
+	// ivent for row removing
+	table.addEventListener('click', eventRowRemovin);
+
+	// init function append inputs
+	appendInput();
+
+	// init function adding events for sort table
+	eventSortTable();
+
+	// init function adding events for edit table cell
+	eventEditTable();
 
 })();
