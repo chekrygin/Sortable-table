cc633c0349d323fb186569e84733249434c9d12a
ochekrigin <chekriginoleg@gmail.com>
2016-08-22 15:13:24 +0300

    Removed old code,  added comments

--- js/app.js
+++ js/app.js
@@ -7,10 +7,17 @@
 		textBlockCollection = document.querySelectorAll('.text'),
 		checkboxCollection = table.querySelectorAll('input[type="checkbox"]'),
 		topClass = 'top',
-		inputCollection,
 		bottomClass = 'bottom',
 		editClass = 'edit';
 
+
+	/**
+	* tableConstructor() sort table constructor
+	* based on the passed parameters
+	*
+	* @param {table} table
+	* @param {textItem} text items collection
+	*/
 
 	var tableConstructor = function (table, textItem) {
 		var self = this;
@@ -28,6 +35,12 @@
 		this.inputCollection = document.querySelectorAll('input[type="text"]');
 	}
 
+	/**
+	* editTable() edit table cells
+	* based on the passed parameters
+	*
+	* @param {collection} input collection
+	*/
 	tableConstructor.prototype.editTable = function (collection) {
 		for (var i = 0; i < collection.length; i++) {
 			(function (i) {
@@ -43,6 +56,12 @@
 		}
 	}
 
+	/**
+	* rowRemove() remove table rows
+	* based on the passed parameters
+	*
+	* @param {collection} row collection
+	*/
 	tableConstructor.prototype.rowRemove = function (collection) {
 		for (var i = 0; i < collection.length; i++) {
 			var item = collection[i];
@@ -52,6 +71,13 @@
 		}
 	}
 
+	/**
+	* sort() sort table
+	* based on the passed parameters
+	*
+	* @param {event} event
+	* @param {target} target
+	*/
 	tableConstructor.prototype.sort = function (event, target) {
 		var self = this;
 		[].forEach.call(tableTh, function (th, i) {
@@ -72,6 +98,7 @@
             arrayCols[i].innerHTML = name;
         });
 
+		//function for data sort
 		function sortData(elements) {
 	        var data = [];
 
@@ -94,6 +121,12 @@
 
 	}
 
+	/**
+	* inputEdit() edit table
+	* based on the passed parameters
+	*
+	* @param {event} event
+	*/
 	tableConstructor.prototype.inputEdit = function (event) {
 		var currentCell = event.target.closest('td');
 		if (currentCell) {
@@ -105,16 +138,21 @@
 		}
 	}
 
+	//function adding events for sort table
 	var sortTable = new tableConstructor(table, textBlockCollection);
 
+	//adding event for inputs showing
 	table.addEventListener('dblclick', sortTable.inputEdit);
 
+	//function edit table
 	sortTable.editTable(sortTable.inputCollection);
 
+	//adding event for row remove
 	btnDelete.onclick = function () {
 	  sortTable.rowRemove(checkboxCollection);
 	};
 
+    //adding event for sort table
     table.addEventListener('click', function (event) {
     	var targetElemt = event.target;
     	if (targetElemt.tagName === 'TH') {
@@ -129,120 +167,4 @@
             }
     	}
     });
-
-	/**
-	* sortTable() sort table's cell
-	* based on the passed parameters
-	*
-	* @param {item} table
-	* @param {item} col
-	* @param {number} reverse
-	*/
-	// function sortTable(table, col, reverse) {
-	//     var tbody = table.tBodies[0],
-	//         tr = Array.prototype.slice.call(tbody.rows, 0),
-	//         i;
-
-	//     reverse = -((+reverse) || -1);
-	//     tr = tr.sort(function (a, b) {
-	//         return reverse * (a.cells[col].textContent.trim().localeCompare(b.cells[col].textContent.trim()));
-	//     });
-
-	//     for (i = 0; i < tr.length; ++i) {
-	//     	tbody.appendChild(tr[i]);
-	//     }
-	// }
-
-	/**
-	* removeItem() remove table rows
-	* based on the passed parameters
-	*
-	* @param {item} item
-	*/
-	// function removeItem (item) {
-	// 	if (item) {
-	// 		var curentRow = item.closest('tr'),
-	// 			currentCheckbox = curentRow.querySelector('input[type="checkbox"]');
-	// 		if (currentCheckbox.checked) {
-	// 			curentRow.remove();
-	// 		}
-	// 	}
-	// }
-
-	// function appendInput () {
-	// 	for (var i = 0; i < textBlockCollection.length; i++) {
-	// 		var inputClone = input.cloneNode(true);
-	// 		textBlockCollection[i].appendChild(inputClone);
-	// 	}
-	// 	inputCollection = document.querySelectorAll('input[type="text"]');
-	// }
-
-	//function adding events for sort table
-	// function eventSortTable () {
-	// 	for (var j = tCells.length - 1; j >= 0 ; j--) {
-	// 		var currentItem = tCells[j],
-	// 			dir = 1;
-	// 		(function (j){
-	// 			currentItem.addEventListener('click', function () {
-	// 				if (this.classList.contains(topClass)) {
-	// 					this.classList.remove(topClass);
-	// 					this.classList.add(bottomClass);
-	// 				} else {
-	// 					this.classList.remove(bottomClass);
-	// 					this.classList.add(topClass);
-	// 				}
-	// 	        	sortTable(table, j, (dir = 1 - dir))
-	// 	        });
-	// 		})(j)
-	// 	}
-	// }
-
-	// function adding events for edit table cell
-	// function eventEditTable () {
-	// 	for (var i = 0; i < inputCollection.length; i++) {
-	// 		var currentInput = inputCollection[i];
-	// 		currentInput.addEventListener('focus', function () {
-	// 			this.value = currentCell.getElementsByTagName('span')[0].innerText;
-	// 		});
-	// 		currentInput.addEventListener('blur', function () {
-	// 			currentCell.getElementsByTagName('span')[0].innerText = this.value;
-	// 			currentCell.classList.remove(editClass);
-	// 		});
-	// 	}
-	// }
-
-	// function removing row from table
-	// function eventInputEdit (event) {
-	// 	currentCell = event.target.closest('td');
-	// 	if (currentCell) {
-	// 		var currentInput = currentCell.querySelectorAll('input[type="text"]')[0];
-	// 		if (currentInput) {
-	// 			currentCell.classList.add(editClass);
-	// 			currentInput.focus();
-	// 		}
-	// 	}
-	// }
-
-	// function removing row from table
-	// function eventRowRemovin (event) {
-	// 	if (event.target.className === buttonCollection[0].className) {
-	// 		removeItem(event.target);
-	// 	}
-	// }
-
-	// event for showing edit input
-	// table.addEventListener('dblclick', eventInputEdit);
-
-	// ivent for row removing
-	// table.addEventListener('click', eventRowRemovin);
-
-	// init function append inputs
-	// appendInput();
-
-	// init function adding events for sort table
-	// eventSortTable();
-
-	// init function adding events for edit table cell
-	// eventEditTable();
-
 })();
